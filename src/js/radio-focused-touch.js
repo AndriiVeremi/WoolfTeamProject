5db3fa075bcbd3697c5eb1d695c41170f4e71a4e
Anton <a.chertok@akatech.com.ua>
2023-08-30 14:15:24 +0300

    fields patterns

--- src/js/radio-focused-touch.js
+++ src/js/radio-focused-touch.js
@@ -2,11 +2,9 @@ const submitBtn = document.querySelector('.js-contact-form-submit');
 
 window.addEventListener('keydown', e => {
   const keyCode = e.code;
-
   if (keyCode !== 'Space' && keyCode !== 'Enter' && keyCode !== 'NumpadEnter') return;
 
   const focusedRadio = document.querySelector('.radio:focus, .radio:focus-visible');
-  console.log(focusedRadio);
   if (!focusedRadio) return;
 
   const label = focusedRadio.closest('.js-radio-label');
